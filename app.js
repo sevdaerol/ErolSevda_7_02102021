6dfed79ba9f9b69e6c15627d714aa177741e3a45
sevdaerol <sevda.erol93@gmail.com>
2021-10-14 01:22:54 +0200

    injections SQL

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 
 require('dotenv').config()
-
+//se connecter a la bdd mysql
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : process.env.DB_HOST,
@@ -13,7 +13,7 @@ const connection = mysql.createConnection({
   password : process.env.DB_PASS,
   database : process.env.DATABASE
 });
-
+//lancer la connection mysql
 connection.connect ((err) => { //se connecter
     if (err) {
       console.log ('Erreur de connexion à Mysql');
@@ -21,22 +21,30 @@ connection.connect ((err) => { //se connecter
     console.log ('Connexion à Mysql');  
     }
 });
-
+//recuperer donnees de la table user
 connection.query ('SELECT * FROM user', (err, lignes) => {
     if (err) throw err;
   
     console.log ('Données reçues de user:');
     console.log (lignes);
 });
-
+//recuperer donnees de la table message
 connection.query ('SELECT * FROM message', (err, lignes) => {
     if (err) throw err;
   
     console.log ('Données reçues de message:');
     console.log (lignes);
 });
+
+//eviter attaques par injections SQL
+var userId = 'some user provided value';
+var sql    = 'SELECT * FROM user WHERE id = ' + connection.escape(userId);
+connection.query(sql, function (error, results, fields) {
+  if (error) throw error;
+  // ...
+});
  
-connection.end();
+connection.end();//terminer la connection
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
